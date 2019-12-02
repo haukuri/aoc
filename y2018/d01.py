8a9d1ade66b57f5efb5444eb555a678e79bc2509
Haukur Ingi Heidarsson <haukur.heidarsson@gmai.com>
2019-12-02 23:29:55 +0000

    2018 Day 1 part II

--- y2018/d01.py
+++ y2018/d01.py
@@ -48,7 +48,60 @@ Here are other example situations:
 Starting with a frequency of zero, what is the resulting frequency after all of
 the changes in frequency have been applied?
 
+--- Part Two ---
+
+You notice that the device repeats the same frequency change list over and over.
+To calibrate the device, you need to find the first frequency it reaches twice.
+
+For example, using the same list of changes above, the device would loop as
+follows:
+
+    Current frequency  0, change of +1; resulting frequency  1.
+    Current frequency  1, change of -2; resulting frequency -1.
+    Current frequency -1, change of +3; resulting frequency  2.
+    Current frequency  2, change of +1; resulting frequency  3.
+    (At this point, the device continues from the start of the list.)
+    Current frequency  3, change of +1; resulting frequency  4.
+    Current frequency  4, change of -2; resulting frequency  2, which has 
+    already been seen.
+
+In this example, the first frequency reached twice is 2. Note that your device
+might need to repeat its list of frequency changes many times before a duplicate
+frequency is found, and that duplicates might be found while in the middle of
+processing the list.
+
+Here are other examples:
+
+    +1, -1 first reaches 0 twice.
+    +3, +3, +4, -2, -4 first reaches 10 twice.
+    -6, +3, +8, +5, -6 first reaches 5 twice.
+    +7, +7, -2, -7, -4 first reaches 14 twice.
+
+What is the first frequency your device reaches twice?
+
+
 """
 
-def test_2018_d01():
-    pass
+def cycle(sequence):
+    while True:
+        yield from sequence
+
+def main():
+    import pathlib
+    input_file = pathlib.Path(__file__).parent / 'd01input'
+    changes = [int(l) for l in input_file.open().readlines()]
+    shift = sum(changes)
+    print('Frequency shift:', shift)
+    
+    frequencies = set()
+    f = 0
+    frequencies.add(f)
+    for df in cycle(changes):
+        f += df
+        if f in frequencies:
+            print('First revisited frequency:', f)
+            break
+        frequencies.add(f)
+
+if __name__ == '__main__':
+    main()
