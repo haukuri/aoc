5631afff8fbb473e147e23c33ea9e674d9ffb864
Haukur Ingi Heidarsson <haukur.heidarsson@gmai.com>
2019-12-02 22:21:03 +0000

    Day 2 part II

--- 2019/d02.py
+++ 2019/d02.py
@@ -84,6 +84,54 @@ before the last computer caught fire. To do this, before running the program,
 replace position 1 with the value 12 and replace position 2 with the value 2.
 What value is left at position 0 after the program halts?
 
+--- Part Two ---
+
+"Good, the new computer seems to be working correctly! Keep it nearby during
+this mission - you'll probably use it again. Real Intcode computers support many
+more features than your new one, but we'll let you know what they are as you
+need them."
+
+"However, your current priority should be to complete your gravity assist around
+the Moon. For this mission to succeed, we should settle on some terminology for
+the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the
+initial state for the computer's memory. When you run an Intcode program, make
+sure to start by initializing memory to the program's values. A position in
+memory is called an address (for example, the first value in memory is at
+"address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used
+immediately after an opcode, if any, are called the instruction's parameters.
+For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the
+parameters. The instruction 99 contains only an opcode and has no parameters.
+
+The address of the current instruction is called the instruction pointer; it
+starts at 0. After an instruction finishes, the instruction pointer increases by
+the number of values in the instruction; until you add more instructions to the
+computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply
+instructions. (The halt instruction would increase the instruction pointer by 1,
+but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the
+gravity assist, you need to determine what pair of inputs produces the output
+19690720."
+
+The inputs should still be provided to the program by replacing the values at
+addresses 1 and 2, just like before. In this program, the value placed in
+address 1 is called the noun, and the value placed in address 2 is called the
+verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just
+like before. Each time you try a pair of inputs, make sure you first reset the
+computer's memory to the values in the program (your puzzle input) - in other
+words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output
+19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the
+answer would be 1202.)
+
+
 """
 
 import pathlib
@@ -112,6 +160,21 @@ def evaluate(program):
         state[r_ref] = r
         pc += 4
 
+def load_program():
+    input_file = pathlib.Path(__file__).parent / 'd02input'
+    program = [int(d) for d in input_file.read_text().split(',')]
+    def run(noun, verb):
+        program[1] = noun
+        program[2] = verb
+        state = evaluate(program)
+        return state[0]
+    return run
+
+def evaluate_noun_and_verb(noun, verb):
+    input_file = pathlib.Path(__file__).parent / 'd02input'
+    program = [int(d) for d in input_file.read_text().split(',')]
+    program[1] = 12
+    program[2] = 2
 
 @pytest.mark.parametrize(
     'initial_state, expected_halt_state',
@@ -125,13 +188,23 @@ def test_evaluate(initial_state, expected_halt_state):
     halt_state = evaluate(initial_state)
     assert halt_state == expected_halt_state
 
+
+
 def main():
-    input_file = pathlib.Path(__file__).parent / 'd02input'
-    program = [int(d) for d in input_file.read_text().split(',')]
-    program[1] = 12
-    program[2] = 2
-    halt_state = evaluate(program)
-    print(halt_state[0])
+    program = load_program()
+    print('1202 output', program(12, 2))
+
+    target_output = 19690720
+    import itertools
+    combinations = itertools.product(range(0, 100), range(0, 100))
+    for noun, verb in combinations:
+        output = program(noun, verb)
+        if output == target_output:
+            code = noun * 100 + verb
+            print('Found noun and verb:', code)
+            break
+    else:
+        print('Noun and verb not found')
 
 if __name__ == "__main__":
     main()
