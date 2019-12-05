9f86f251b39d871ad95d393b46f21fe93e77860e
Haukur Ingi Heidarsson <haukur.heidarsson@gmai.com>
2019-12-05 22:56:30 +0000

    2019 day 4 part II

--- y2019/d04.py
+++ y2019/d04.py
@@ -23,6 +23,26 @@ How many different passwords within the range given in your puzzle input meet
 these criteria?
 
 Your puzzle input is 123257-647015.
+
+--- Part Two ---
+
+An Elf just remembered one more important detail: the two adjacent matching
+digits are not part of a larger group of matching digits.
+
+Given this additional criterion, but still ignoring the range rule, the
+following are now true:
+
+    112233 meets these criteria because the digits never decrease and all 
+    repeated digits are exactly two digits long.
+    123444 no longer meets the criteria (the repeated 44 is part of a larger 
+    group of 444).
+    111122 meets the criteria (even though 1 is repeated more than twice, it 
+    still contains a double 22).
+
+How many different passwords within the range given in your puzzle input meet
+all of the criteria?
+
+Your puzzle input is still 123257-647015.
 """
 import math
 
@@ -37,13 +57,28 @@ def digits(number):
         yield component
         remainder = remainder % decimal
 
+def collect(sequence):
+    it = iter(sequence)
+    try:
+        current = next(it)
+        count = 1
+    except StopIteration:
+        return
+    while True:
+        try:
+            next_value = next(it)
+            if next_value == current:
+                count += 1
+            else:
+                yield current, count
+                current = next_value
+                count = 1
+        except StopIteration:
+            yield current, count
+            break
+
 def has_twin_digits(number):
-    last_digit = 0
-    for digit in digits(number):
-        if digit == last_digit:
-            return True
-        last_digit = digit
-    return False
+    return any(count >= 2 for _, count in collect(digits(number)))
 
 def has_increasing_digits(number):
     last_digit = 0
@@ -75,13 +110,52 @@ def test_has_twin_digits(number, expected):
 def test_has_increasing_digits(number, expected):
     assert has_increasing_digits(number) == expected
 
-def main():
+def has_twins_that_are_not_triplets(number):
+    return any(count == 2 for _, count in collect(digits(number)))
+
+@pytest.mark.parametrize(
+    'number, expected',
+    [(112233, True), (123444, False), (111122, True), (122234, False)]
+)
+def test_has_twins_that_are_not_triplets(number, expected):
+    assert has_twins_that_are_not_triplets(number) == expected
+
+def test_collect_works_for_empty_sequences():
+    input = []
+    output = list(collect(input))
+    assert len(output) == 0
+
+def test_collect():
+    input = [7, 8, 8, 9, 9, 9]
+    expected = ((7, 1), (8, 2), (9, 3))
+    actual = tuple(collect(input))
+    assert actual == expected
+
+def part1():
     count = 0
     for candidate in range(123257, 647016):
-
         if has_increasing_digits(candidate) and has_twin_digits(candidate):
             count += 1
-    print('Number of candidates:', count) # 2220
+    return count
+
+def part2():
+    count = 0
+    for candidate in range(123257, 647016):
+        if has_increasing_digits(candidate) \
+            and has_twin_digits(candidate) \
+            and has_twins_that_are_not_triplets(candidate):
+            count += 1
+    return count
+
+def test_part1():
+    assert part1() == 2220
+
+def test_part2():
+    assert part2() == 1515
+
+def main():
+    print('Number of candidates (I):', part1()) # 2220
+    print('Number of candidates (II):', part2()) # 1515
 
 if __name__ == "__main__":
     main()
