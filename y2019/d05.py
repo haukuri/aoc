f9558f5ab347d590ddd7f08d37ddf27d6bb574d6
Haukur Ingi Heidarsson <haukur.heidarsson@gmai.com>
2019-12-14 08:59:38 +0000

    y2019 d05 p2 wip

--- y2019/d05.py
+++ y2019/d05.py
@@ -98,31 +98,61 @@ diagnostic code does the program produce?
 
 --- Part Two ---
 
-The air conditioner comes online! Its cold air feels good for a while, but then the TEST alarms start to go off. Since the air conditioner can't vent its heat anywhere but back into the spacecraft, it's actually making the air inside the ship warmer.
+The air conditioner comes online! Its cold air feels good for a while, but then
+the TEST alarms start to go off. Since the air conditioner can't vent its heat
+anywhere but back into the spacecraft, it's actually making the air inside the
+ship warmer.
 
-Instead, you'll need to use the TEST to extend the thermal radiators. Fortunately, the diagnostic program (your puzzle input) is already equipped for this. Unfortunately, your Intcode computer is not.
+Instead, you'll need to use the TEST to extend the thermal radiators.
+Fortunately, the diagnostic program (your puzzle input) is already equipped for
+this. Unfortunately, your Intcode computer is not.
 
 Your computer is only missing a few opcodes:
 
-    Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-    Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-    Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-    Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+    Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the 
+    instruction pointer to the value from the second parameter. Otherwise, 
+    it does nothing.
+    
+    Opcode 6 is jump-if-false: if the first parameter is zero, it sets the 
+    instruction pointer to the value from the second parameter. Otherwise, it 
+    does nothing.
+    
+    Opcode 7 is less than: if the first parameter is less than the second 
+    parameter, it stores 1 in the position given by the third parameter. 
+    Otherwise, it stores 0.
+    
+    Opcode 8 is equals: if the first parameter is equal to the second parameter, 
+    it stores 1 in the position given by the third parameter. Otherwise, it 
+    stores 0.
 
-Like all instructions, these instructions need to support parameter modes as described above.
+Like all instructions, these instructions need to support parameter modes as
+described above.
 
-Normally, after an instruction is finished, the instruction pointer increases by the number of values in that instruction. However, if the instruction modifies the instruction pointer, that value is used and the instruction pointer is not automatically increased.
+Normally, after an instruction is finished, the instruction pointer increases by
+the number of values in that instruction. However, if the instruction modifies
+the instruction pointer, that value is used and the instruction pointer is not
+automatically increased.
 
-For example, here are several programs that take one input, compare it to the value 8, and then produce one output:
+For example, here are several programs that take one input, compare it to the
+value 8, and then produce one output:
 
-    3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-    3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
-    3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-    3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+    3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input 
+    is equal to 8; output 1 (if it is) or 0 (if it is not).
+    
+    3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input 
+    is less than 8; output 1 (if it is) or 0 (if it is not).
+    
+    3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is 
+    equal to 8; output 1 (if it is) or 0 (if it is not).
+    
+    3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is 
+    less than 8; output 1 (if it is) or 0 (if it is not).
 
-Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+Here are some jump tests that take an input, then output 0 if the input was zero
+or 1 if the input was non-zero:
 
     3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 (using position mode)
+
     3,3,1105,-1,9,1101,0,0,12,4,12,99,1 (using immediate mode)
 
 Here's a larger example:
@@ -131,13 +161,17 @@ Here's a larger example:
 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
 
-The above example program uses an input instruction to ask for a single number. The program will then output 999 if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is greater than 8.
+The above example program uses an input instruction to ask for a single number.
+The program will then output 999 if the input value is below 8, output 1000 if
+the input value is equal to 8, or output 1001 if the input value is greater than
+8.
 
-This time, when the TEST diagnostic program runs its input instruction to get the ID of the system to test, provide it 5, the ID for the ship's thermal radiator controller. This diagnostic test suite only outputs one number, the diagnostic code.
+This time, when the TEST diagnostic program runs its input instruction to get
+the ID of the system to test, provide it 5, the ID for the ship's thermal
+radiator controller. This diagnostic test suite only outputs one number, the
+diagnostic code.
 
 What is the diagnostic code for system ID 5?
-
-
 """
 
 from collections import deque
@@ -145,7 +179,7 @@ from dataclasses import dataclass
 from typing import List, Tuple, Callable, Mapping
 from enum import IntEnum
 
-from utils import read_csv_input
+from .utils import read_csv_input
 
 class Mode(IntEnum):
     POSITION = 0
@@ -156,6 +190,10 @@ class Opcode(IntEnum):
     MULTIPLY = 2
     READ_INPUT = 3
     WRITE_OUTPUT = 4
+    JUMP_IF_TRUE = 5
+    JUMP_IF_FALSE = 6
+    LESS_THAN = 7
+    EQUALS = 8
     HALT = 99
 
 @dataclass
@@ -191,6 +229,9 @@ class Computer:
     input: deque
     output: List[int]
 
+    def __init__(self, debug=False):
+        self.debug = bool(debug)
+
     def load(self, address: int, mode: Mode):
         value = self.memory[address]
         if (mode == Mode.IMMEDIATE):
@@ -221,46 +262,84 @@ class Computer:
     
     def handle(self, instruction: Instruction):
         opcode = instruction.opcode
-        print(opcode.name)
+        if self.debug:
+            print(opcode.name)
         opcode_name = opcode.name.lower()
         handler = getattr(self, f'handle_{opcode_name}')
-        handler(instruction)
+        handler(instruction.modes)
     
-    def handle_add(self, instruction: Instruction):
-        a_mode, b_mode, r_mode  = instruction.modes
+    def handle_add(self, modes: Tuple[Mode]):
+        a_mode, b_mode, r_mode  = modes
         a = self.load(self.pc+1, a_mode)
         b = self.load(self.pc+2, b_mode)
         r = a + b
         self.store(r, self.pc+3, r_mode)
         self.pc += 4
     
-    def handle_multiply(self, instruction: Instruction):
-        a_mode, b_mode, r_mode  = instruction.modes
+    def handle_multiply(self, modes: Tuple[Mode]):
+        a_mode, b_mode, r_mode  = modes
         a = self.load(self.pc+1, a_mode)
         b = self.load(self.pc+2, b_mode)
         r = a * b
         self.store(r, self.pc+3, r_mode)
         self.pc += 4
     
-    def handle_read_input(self, instruction: Instruction):
+    def handle_read_input(self, modes: Tuple[Mode]):
         value = self.input.popleft()
-        mode = instruction.modes[0]
+        mode = modes[0]
         self.store(value, self.pc+1, mode)
         self.pc += 2
     
-    def handle_write_output(self, instruction: Instruction):
-        mode = instruction.modes[0]
+    def handle_write_output(self, modes: Tuple[Mode]):
+        mode = modes[0]
         value = self.load(self.pc+1, mode)
         self.output.append(value)
         self.pc += 2
 
-    def handle_halt(self, instruction: Instruction):
+    def handle_jump_if_true(self, modes: Tuple[Mode]):
+        preditcate = self.load(self.pc+1, modes[0])
+        target = self.load(self.pc+2, modes[1])
+        if preditcate:
+            self.pc = target
+        else:
+            self.pc += 3
+    
+    def handle_jump_if_false(self, modes: Tuple[Mode]):
+        preditcate = self.load(self.pc+1, modes[0])
+        target = self.load(self.pc+2, modes[1])
+        if not preditcate:
+            self.pc = target
+        else:
+            self.pc += 3
+        
+    def handle_less_than(self, modes: Tuple[Mode]):
+        a = self.load(self.pc+1, modes[0])
+        b = self.load(self.pc+2, modes[1])
+        result = 1 if a < b else 0
+        self.store(result, self.pc+3, modes[2])
+    
+    def handle_equals(self, modes: Tuple[Mode]):
+        a = self.load(self.pc+1, modes[0])
+        b = self.load(self.pc+2, modes[1])
+        result = 1 if a == b else 0
+        self.store(result, self.pc+3, modes[2])
+
+    def handle_halt(self, modes: Tuple[Mode]):
         raise HaltExecution
+
+def test_computer():
+    program = [
+        3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+        1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+        999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+    input = [7]
+    computer = Computer()
+    computer.evaluate(program, input=input)
+    assert computer.output[0] == 999
 
 def main():
     input_data = read_csv_input('d05input')
     program = input_data[0]
-    print(input_data)
     computer = Computer()
     input = [1]
     computer.evaluate(program, input=input)
