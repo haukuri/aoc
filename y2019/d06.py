60be742b0f6161846342fcf5c11357b9db2184dc
Haukur Ingi Heidarsson <haukur.heidarsson@gmai.com>
2019-12-15 22:53:21 +0000

    y2019 d06 part I done

--- y2019/d06.py
+++ y2019/d06.py
@@ -71,4 +71,92 @@ The total number of direct and indirect orbits in this example is 42.
 
 What is the total number of direct and indirect orbits in your map data?
 
+--- Part Two ---
+
+Now, you just need to figure out how many orbital transfers you (YOU) need to 
+take to get to Santa (SAN).
+
+You start at the object YOU are orbiting; your destination is the object SAN is 
+orbiting. An orbital transfer lets you move from any object to an object 
+orbiting or orbited by that object.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+
+Visually, the above map of orbits looks like this:
+
+                          YOU
+                         /
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+
+In this example, YOU are in orbit around K, and SAN is in orbit around I. To 
+move from K to I, a minimum of 4 orbital transfers are required:
+
+    K to J
+    J to E
+    E to D
+    D to I
+
+Afterward, the map of orbits looks like this:
+
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+                 \
+                  YOU
+
+What is the minimum number of orbital transfers required to move from the object 
+YOU are orbiting to the object SAN is orbiting? (Between the objects they are 
+orbiting - not between YOU and SAN.)
+
+
 """
+
+import pathlib
+import collections
+from pprint import pprint
+
+
+
+def count_orbits(filename):
+    input_path = pathlib.Path(__file__).parent / filename
+    orbits = {}
+    for line in input_path.open('r').readlines():
+        a, b = line.strip().split(')')
+        orbits[b] = a
+    pairs = set()
+    for orbiter, orbitee in orbits.items():
+        while orbitee:
+            pairs.add((orbiter, orbitee))
+            orbitee = orbits.get(orbitee, None)
+    return len(pairs)
+
+def test_count_orbits():
+    actual = count_orbits('d06input.test')
+    assert actual == 42
+
+def main():
+    num_orbits = count_orbits('d06input')
+    print('Number of orbits:', num_orbits) # 194721
+
+if __name__ == "__main__":
+    main()
