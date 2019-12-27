8b87e71d90d1f753b68dc0c377169ec51818c846
Haukur Ingi Heidarsson <haukur.heidarsson@gmai.com>
2019-12-27 09:54:23 +0000

    Fixed line wrapping

--- y2019/d05.py
+++ y2019/d05.py
@@ -12,7 +12,8 @@ your existing Intcode computer after a few modifications:
 First, you'll need to add two new instructions:
 
     Opcode 3 takes a single integer as input and saves it to the position given 
-    by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
+    by its only parameter. For example, the instruction 3,50 would take an input 
+    value and store it at address 50.
     
     Opcode 4 outputs the value of its only parameter. For example, the 
     instruction 4,50 would output the value at address 50.
