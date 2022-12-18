00da4d0fa4a2bd81adb33683012c250c32aec79c
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2022-12-18 22:46:55 +0000

    Day 4 2022 Part 1

---
+++ aoc/y2022/d04.py
@@ -0,0 +1,45 @@
+import utils
+
+example_input = """
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8""".strip()
+
+class Assignment:
+    def __init__(self, start: int, end: int):
+        self.start = start
+        self.end = end
+    
+    @classmethod
+    def from_encoded_range(cls, encoded_range: str):
+        s, e = encoded_range.split("-")
+        s, e = int(s), int(e)
+        return cls(s, e)
+
+    def fully_contains(self, other: "Assignment") -> bool:
+        return self.start <= other.start and self.end >= other.end
+
+    
+
+def solve_part_1(input: str):
+    num_full_containments = 0
+    for line in input.splitlines():
+        a, b = line.split(",")
+        assignment_a = Assignment.from_encoded_range(a)
+        assignment_b = Assignment.from_encoded_range(b)
+        full_containment = assignment_a.fully_contains(assignment_b) \
+            or assignment_b.fully_contains(assignment_a)
+        if full_containment:
+            num_full_containments += 1
+    return num_full_containments
+        
+
+def test_part_1_example():
+    assert solve_part_1(example_input) == 2
+
+def test_part_1_actual():
+    input = utils.read_text("d04input.txt")
+    assert solve_part_1(input) == 487
