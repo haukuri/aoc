3cf780f740575097f05fb1c307d869d239c82292
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2022-12-21 22:32:30 +0000

    Day 6 2022 Part 2

--- aoc/y2022/d06.py
+++ aoc/y2022/d06.py
@@ -2,17 +2,24 @@ import pytest
 
 import utils
 
-examples = [
+examples_part_1 = [
     ("bvwbjplbgvbhsrlpgdmjqwftvncz", 5),
     ("nppdvjthqldpwncqszvftbrmjlhg", 6),
     ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 10),
     ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 11)
 ]
 
+examples_part_2 = [
+    ("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 19),
+    ("bvwbjplbgvbhsrlpgdmjqwftvncz", 23),
+    ("nppdvjthqldpwncqszvftbrmjlhg", 23),
+    ("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 29),
+    ("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 26)
+]
 
-def solve_part_1(input: str) -> int:
-    for start_idx in range(0, len(input) - 4):
-        end_idx = start_idx + 4
+def find_end_of_first_unique_substring(input: str, length: int) -> int:
+    for start_idx in range(0, len(input) - length):
+        end_idx = start_idx + length
         section = input[start_idx:end_idx]
         seen = set()
         has_duplicates = False
@@ -23,8 +30,13 @@ def solve_part_1(input: str) -> int:
             return end_idx
     return -1
 
+def solve_part_1(input: str) -> int:
+    return find_end_of_first_unique_substring(input, 4)
 
-@pytest.mark.parametrize("input, expected", examples)
+def solve_part_2(input: str) -> int:
+    return find_end_of_first_unique_substring(input, 14)
+
+@pytest.mark.parametrize("input, expected", examples_part_1)
 def test_solve_part_1_example(input, expected):
     actual = solve_part_1(input)
     assert actual == expected
@@ -33,3 +45,13 @@ def test_solve_part_1_input():
     input = utils.read_text("d06input.txt")
     actual = solve_part_1(input)
     assert actual == 1757
+
+@pytest.mark.parametrize("input, expected", examples_part_2)
+def test_solve_part_2_example(input, expected):
+    actual = solve_part_2(input)
+    assert actual == expected
+
+def test_solve_part_2_input():
+    input = utils.read_text("d06input.txt")
+    actual = solve_part_2(input)
+    assert actual == 2950
