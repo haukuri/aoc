fd3e63093feb0a537892817ec967834154f25f56
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2022-12-29 13:22:58 +0000

    Day 9 2022 Part 2

--- aoc/y2022/d09.py
+++ aoc/y2022/d09.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 
 import utils
 
-example_input = """R 4
+example_input_a = """R 4
 U 4
 L 3
 D 1
@@ -10,6 +10,16 @@ R 4
 D 1
 L 5
 R 2"""
+
+example_input_b = """R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20
+"""
 
 @dataclass(frozen=True)
 class Vector2D:
@@ -58,6 +68,22 @@ def sign(x: int):
     else:
         raise ValueError
 
+def move_tail(h, t):
+    d = h - t
+    if are_adjacent(h, t):
+        nt = t
+    elif d.x == 0: # up / down
+        assert abs(d.y) == 2
+        nt =  t + Vector2D(0, sign(d.y))
+    elif d.y == 0: # right / left
+        assert abs(d.x) == 2
+        nt =  t + Vector2D(sign(d.x), 0)
+    else: # diagonal
+        nt =  t + Vector2D(sign(d.x), sign(d.y))
+        assert (h - nt).manhattan_distance() <= 3
+    assert are_adjacent(h, nt)
+    return nt
+
 def solve_part_1(input: str):
     directions = parse_input(input)
     s = Vector2D(0, 0)
@@ -66,23 +92,25 @@ def solve_part_1(input: str):
     t_path = { t }
     for step in directions:
         h += step
-        d = h - t
-        if are_adjacent(h, t):
-            continue
-        if d.x == 0: # up / down
-            assert abs(d.y) == 2
-            t += Vector2D(0, sign(d.y))
-        elif d.y == 0: # right / left
-            assert abs(d.x) == 2
-            t += Vector2D(sign(d.x), 0)
-        else: # diagonal
-            assert d.manhattan_distance() <= 3
-            t += Vector2D(sign(d.x), sign(d.y))
+        t = move_tail(h, t)
         t_path.add(t)
-        assert are_adjacent(h, t)
     return len(t_path)
 
-def test_parse_input_example():
+def solve_part_2(input: str):
+    directions = parse_input(input)
+    s = Vector2D(0, 0)
+    n = 10
+    rope = [s for _ in range(n)]
+    t_path = { s }
+    for step in directions:
+        rope[0] += step
+        for k in range(1, n):
+            rope[k] = move_tail(rope[k-1], rope[k])
+        t_path.add(rope[-1])
+    return len(t_path)
+
+
+def test_parse_input_example_a():
     r = Vector2D(1, 0)
     l = Vector2D(-1, 0)
     u = Vector2D(0, 1)
@@ -97,11 +125,21 @@ def test_parse_input_example():
         l, l, l, l, l,
         r, r,
     ]
-    assert parse_input(example_input) == expected
+    assert parse_input(example_input_a) == expected
 
-def test_solve_part_1_example():
-    assert solve_part_1(example_input) == 13
+def test_solve_part_1_example_a():
+    assert solve_part_1(example_input_a) == 13
 
 def test_solve_part_1_actual():
     actual_input = utils.read_text("d09input.txt")
     assert solve_part_1(actual_input) == 6332
+
+def test_solve_part_2_example_a():
+    assert solve_part_2(example_input_a) == 1
+
+def test_solve_part_2_example_b():
+    assert solve_part_2(example_input_b) == 36
+
+def test_solve_part_2_actual():
+    actual_input = utils.read_text("d09input.txt")
+    assert solve_part_2(actual_input) == 2511
