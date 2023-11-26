bbbf3d8e5ab4cff0735ea835d34d4b566a644676
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2023-11-26 23:02:19 +0000

    .NET solution

---
+++ net/src/Year2022/Day01.cs
@@ -0,0 +1,54 @@
+﻿namespace AdventOfCode.Year2022
+{
+    public class Day01 : ISolution<Day01Solution>
+    {
+        public Day01Solution Solve(TextReader reader)
+        {
+            var currentCalories = 0;
+            var maxCalories = 0;
+            var maxCalorieHolder = 0;
+            var elfNumber = 1;
+
+            foreach (var line in reader.IterLines())
+            {
+                if (string.IsNullOrEmpty(line))
+                {
+                    currentCalories = 0;
+                    elfNumber++;
+                }
+                else
+                {
+                    currentCalories += Convert.ToInt32(line);
+                    if (currentCalories > maxCalories)
+                    {
+                        maxCalories = currentCalories;
+                        maxCalorieHolder = elfNumber;
+                    }
+                }
+            }
+
+            var solution = new Day01Solution(maxCalories, maxCalorieHolder);
+            return solution;
+        }
+    }
+
+    public record Day01Solution(int MaxCalories, int ElfNumber);
+
+    public interface ISolution<T>
+    {
+        T Solve(TextReader reader);
+    }
+
+    public static class TextReaderExtensions
+    {
+        public static IEnumerable<string> IterLines(this TextReader reader)
+        {
+            while (true)
+            {
+                var line = reader.ReadLine();
+                if (line == null) break;
+                yield return line;
+            }
+        }
+    }
+}
---
+++ net/test/UnitTests/Year2022/Day01Tests.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace AdventOfCode.Year2022
+{
+    [TestClass]
+    public class Day01Tests
+    {
+
+        [TestMethod]
+        public void TestPart1WithExampleInput()
+        {
+            var solver = new Day01();
+            var reader = TestDataReader.Read("Y2023D01P01E.txt");
+            var solution = solver.Solve(reader);
+
+            Assert.AreEqual(4, solution.ElfNumber);
+            Assert.AreEqual(24_000, solution.MaxCalories);
+        }
+
+    }
+
+    public class TestDataReader
+    {
+        public static TextReader Read(string name)
+        {
+            var assemblyPath = Assembly.GetExecutingAssembly().Location;
+            var assemblyDirectory = Path.GetDirectoryName(assemblyPath)!;
+            var testDataFolder = Path.Combine(assemblyDirectory, "TestData");
+            var filePath = Path.Combine(testDataFolder, name);
+            var reader = new StreamReader(filePath);
+            return reader;
+        }
+    }
+}
