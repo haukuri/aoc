cca17b24ac811a75311a444b2c3282d8f437713b
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2023-11-26 23:28:29 +0000

    Rename test data for 2022 day 1

--- net/test/UnitTests/Year2022/Day01Tests.cs
+++ net/test/UnitTests/Year2022/Day01Tests.cs
@@ -3,11 +3,13 @@
     [TestClass]
     public class Day01Tests
     {
+        private const string ExampleInputData = "Y2022D01P01E.txt";
+        private const string ActualInputData = "Y2022D01.txt";
 
         [TestMethod]
         public void TestPart1WithExampleInput()
         {
-            var reader = TestDataReader.Read("Y2023D01P01E.txt");
+            var reader = TestDataReader.Read(ExampleInputData);
             var solution = Day01.SolvePart01(reader);
 
             Assert.AreEqual(24_000, solution);
@@ -16,7 +18,7 @@
         [TestMethod]
         public void TestPart1WithActualInput()
         {
-            var reader = TestDataReader.Read("Y2023D01.txt");
+            var reader = TestDataReader.Read(ActualInputData);
             var solution = Day01.SolvePart01(reader);
 
             Assert.AreEqual(71124, solution);
@@ -25,7 +27,7 @@
         [TestMethod]
         public void TestPart2WithExampleInput()
         {
-            var reader = TestDataReader.Read("Y2023D01P01E.txt");
+            var reader = TestDataReader.Read(ExampleInputData);
             var solution = Day01.SolvePart02(reader);
 
             Assert.AreEqual(45_000, solution);
@@ -34,7 +36,7 @@
         [TestMethod]
         public void TestPart2WithActualInput()
         {
-            var reader = TestDataReader.Read("Y2023D01.txt");
+            var reader = TestDataReader.Read(ActualInputData);
             var solution = Day01.SolvePart02(reader);
 
             Assert.AreEqual(204_639, solution);
