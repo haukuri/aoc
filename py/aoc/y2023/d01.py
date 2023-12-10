5e1f854fe8fb1978d7fd6d95dccc5aaa4d74de12
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2023-12-10 00:13:11 +0000

    2023 01 Part 1

--- py/aoc/y2023/d01.py
+++ py/aoc/y2023/d01.py
@@ -1,3 +1,5 @@
+import utils
+
 example_input = """
 1abc2
 pqr3stu8vwx
@@ -7,4 +9,34 @@ treb7uchet
 
 
 def solve_part_1(input_data: str) -> int:
-    pass
+    acc = 0
+    for line in input_data.splitlines():
+        first_found = False
+        first_digit = 0
+        second_digit = 0
+        for char in line:
+            try:
+                digit = int(char)
+            except ValueError:
+                continue
+            if not first_found:
+                first_digit = digit
+                second_digit = digit
+                first_found = True
+            else:
+                second_digit = digit
+        value = 10 * first_digit + second_digit
+        acc += value
+        print(value)
+    return acc
+
+
+def test_solve_part_1_example():
+    actual = solve_part_1(example_input)
+    assert actual == 142
+
+
+def test_solve_part_1_input():
+    input_text = utils.read_text("d01_input.txt")
+    actual = solve_part_1(input_text)
+    assert actual == 54450
