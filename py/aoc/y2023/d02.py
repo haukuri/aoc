3b02089107248c5357ff54faa863e41ba25e43cc
Haukur Ingi Heiðarsson <haukur@heidarsson.is>
2023-12-29 23:08:35 +0000

    2023 02 part 2

--- py/aoc/y2023/d02.py
+++ py/aoc/y2023/d02.py
@@ -34,10 +34,14 @@ def parse_game_line(line: str) -> Game:
     return Game(id=game_id, turns=turns)
 
 
+def parse_input(input_data: str) -> list[Game]:
+    return [parse_game_line(line) for line in input_data.splitlines()]
+
+
 def solve_part_1(input_data: str) -> int:
     "12 red cubes, 13 green cubes, and 14 blue cubes"
     target_max_colors = {"red": 12, "green": 13, "blue": 14}
-    games = [parse_game_line(line) for line in input_data.splitlines()]
+    games = parse_input(input_data)
     possible_game_ids = []
     for game in games:
         max_color_counts = {}
@@ -51,6 +55,43 @@ def solve_part_1(input_data: str) -> int:
         if possible:
             possible_game_ids.append(game.id)
     return sum(possible_game_ids)
+
+
+def game_power_set(game: Game) -> int:
+    min_color_counts = {}
+    for turn in game.turns:
+        for color, count in turn.items():
+            existing_min = min_color_counts.get(color, 0)
+            min_color_counts[color] = max(existing_min, count)
+    power_set = 1
+    for count in min_color_counts.values():
+        power_set *= count
+    return power_set
+
+
+def solve_part_2(input_data: str) -> int:
+    games = parse_input(input_data)
+    game_power_sets = [game_power_set(game) for game in games]
+    return sum(game_power_sets)
+
+
+def test_solve_part_2_example():
+    actual = solve_part_2(example_input)
+    assert actual == 2286
+
+
+def test_solve_part_2_actual():
+    actual_input = utils.read_text("d02_input.txt")
+    actual = solve_part_2(actual_input)
+    assert actual == 56322
+
+
+def test_game_power_set():
+    game_line = (
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"
+    )
+    game = parse_game_line(game_line)
+    assert game_power_set(game) == 20 * 13 * 6
 
 
 def test_parse_game_line():
